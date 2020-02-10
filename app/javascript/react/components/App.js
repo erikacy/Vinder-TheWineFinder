ecf646271006088097c8b53a1ce9ece9d3cbb6bd
erikacy <erika.cy@gmail.com>
2020-02-10 18:03:55 -0500

    created user log in form

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-export const App = (props) => {
-  return (<h1>The Wine Finder</h1>)
+export const App = () => {
+  return (
+    <h1>The Wine Finder</h1>
+  )
 }
 
 export default App
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+
+  get '/', to: "static_pages#index"
+  get '/wines', to: "static_pages#index"
+  get '/wines/:id', to: "static_pages#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
